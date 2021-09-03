263254af66d3bbaa0656ad67977b009832873322
SpyCooper <jm578220@ohio.edu>
2021-09-03 18:04:22 -0400

    Initial Commit

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Josh Myers
+ *        Date: Sep 3, 2021
+ * Description: The labe we did on the above date, introducing us to git hub/
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
